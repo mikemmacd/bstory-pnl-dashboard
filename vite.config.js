7387deb361e116732eabf52971509ee8cc5e9fba
mikemmacd <mikemmacd@gmail.com>
2025-07-14 19:14:50 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  css: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
 })
